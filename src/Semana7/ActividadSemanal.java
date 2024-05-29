1a6d3fdb5699d1027bfd3442a2847c9ceff75476
Ivan Budzaj <budzajivan@gmail.com>
2024-05-29 13:42:03 -0300

    a

--- src/Semana7/ActividadSemanal.java
+++ src/Semana7/ActividadSemanal.java
@@ -1,5 +1,8 @@
 package Semana7;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ActividadSemanal {
 
 	
@@ -7,4 +10,38 @@ public class ActividadSemanal {
 	public int horaDeInicio;
 	public int duracion;
 	public Deporte  deporte;
+	//		List<ActividadSemanal> listActividadesSem =  Arrays.asList();
+
+	public ActividadSemanal(DiaDeLaSemana dia, int horaInicio, int duracion, Deporte deporte) {
+        this.dia = dia;
+        this.horaDeInicio = horaInicio;
+        this.duracion = duracion;
+        this.deporte = deporte;
+    }
+	
+	public DiaDeLaSemana getDia() {
+		return dia;
+	}
+
+	public int getHoraDeInicio() {
+		return horaDeInicio;
+	}
+
+	public int getDuracion() {
+		return duracion;
+	}
+
+	public Deporte getDeporte() {
+		return deporte;
+	}
+
+	public static List<ActividadSemanal> todaslasDeFutbol(List<ActividadSemanal> dias){
+
+		return dias.stream().filter(a -> a.getDeporte()== Deporte.FUTBOL).toList();
+	 }
+
+	public static List<ActividadSemanal> porComplejidad(List<ActividadSemanal> actvs, int i) {
+		// TODO Auto-generated method stub
+		return actvs.stream().filter(a -> a.getDeporte().complejidad == i).toList();
+	}
 }
--- src/Semana7/Deporte.java
+++ src/Semana7/Deporte.java
@@ -1,5 +1,12 @@
 package Semana7;
 
 public enum Deporte {
-
+	RUNNING(1),FUTBOL(2),BASKET(2),TENNIS(3),JABALINA(4);
+	public int getComplejidad() {
+		return complejidad;
+	}
+	int complejidad;
+	private Deporte(int complejidad) {
+		this.complejidad = complejidad;
+	}
 }
