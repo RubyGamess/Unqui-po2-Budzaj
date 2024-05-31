c99275b3a535b1ab02e8fe68bbb4e59ca65571d7
Ivan Leandro budzaj <98992765+RubyGamess@users.noreply.github.com>
2024-05-31 01:26:08 -0300

    Delete src directory

--- src/tp5/Empresa.java
+++
@@ -1,8 +0,0 @@
-package tp5;
-
-
-public interface Empresa {
-	
-	public float precio(Producto p);
-	
-}
--- src/tp6/Credito.java
+++
@@ -1,6 +0,0 @@
-package tp6;
-
-public interface Credito {
-
-	public Boolean evaluarCredito(Cliente cliente, int monto,int  plazoMeses);
-}
